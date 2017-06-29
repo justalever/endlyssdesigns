ead8730a59504c42d69ce086209e33d818f5b0d5
Andy Leverenz <andy@justalever.com>
2017-06-29 18:45:44 -0500

    Refined global header desktop styles

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -1,7 +1,7 @@
 <nav class="navigation" role="navigation">
   <ul class="menu">
     <?php foreach($pages->visible() as $item): ?>
-    <li class="menu-item<?= r($item->isOpen(), ' is-active') ?>">
+    <li class="menu-item<?= r($item->isOpen(), ' is-active') ?> <?= $item->slug() ?>">
       <a href="<?= $item->url() ?>"><?= $item->title()->html() ?></a>
     </li>
     <?php endforeach ?>
